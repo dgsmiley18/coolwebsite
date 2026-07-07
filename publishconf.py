fe3dae306c8fb2c32c5f25ad1ed72fc6e8d095e2
Douglas Vianna <38007632+dgsmiley18@users.noreply.github.com>
2026-07-06 23:43:57 -0300

    fix: remove trailing slash from SITEURL in publish configuration

--- publishconf.py
+++ publishconf.py
@@ -5,7 +5,7 @@ sys.path.insert(0, str(Path(__file__).resolve().parent))
 
 from pelicanconf import *
 
-SITEURL = "https://johntitorr.org/"
+SITEURL = "https://johntitorr.org"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
